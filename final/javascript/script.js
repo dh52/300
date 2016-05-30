663822ca5c6adca7fb48ad0bf62630bcde8a5084
dh52 <danielh65@comcast.net>
2016-05-29 20:03:44 -0700

    new changes

--- final/javascript/script.js
+++ final/javascript/script.js
@@ -1,8 +1,32 @@
-$(document).ready(function() {
-    $("#storm").css("opacity", 0.5);
-    $("#storm").hover(function() {
-        $(this).animate({opacity: 1.0}, 500);
-    }, function() {
-        $(this).animate({opacity: 0.5}, 500);
-    });
-});
+var map;
+var zoomVal = 16;
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  
+    map = new google.maps.Map(mapDiv, {
+    center: {lat: 47.651371, lng: -122.349929},
+    zoom: 16
+  });
+
+  var marker = new google.maps.Marker ( {
+    position: {lat: 47.651371, lng: -122.349929}
+  } );
+  
+  marker.setMap(map);
+  var infoWindow = new google.maps.InfoWindow( {
+    content: 'Fremont Analytical'
+  } );
+  
+  marker.addListener( 'click', function( ) {
+    infoWindow.open( map, marker );
+  } );
+  }
+
+function moveTo() {
+  var panPoint = new google.maps.LatLng(document.getElementById("lat").value,
+                                        document.getElementById("lng").value);
+  zoomVal = document.getElementById("zoom").value; 
+  map.setZoom(zoomVal);     
+
+  map.panTo(panPoint);
+     }
